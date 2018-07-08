2ad7ed8cc0ae116ab269f18ddf520b195ce52802
ladingmumu <295741509@qq.com>
2018-07-08 08:56:04 +0000

    Finish basic page layout

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    //处理首页的展示
+    public function root(){
+        return view('pages.root');
+    }
+}
