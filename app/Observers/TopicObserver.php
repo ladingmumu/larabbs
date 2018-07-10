b2cc9712a508565937fe16f9233736e2ae06d7a6
ladingmumu <295741509@qq.com>
2018-07-10 04:14:09 +0000

    Add queue for translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,7 +3,8 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-use App\Handlers\SlugTranslateHandler;
+// use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug; //将 Slug 翻译的调用修改为队列执行的方式
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -30,10 +31,19 @@ class TopicObserver
 
         // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
+    }
+
+
+
+    //模型监控器的 saved() 方法对应 Eloquent 的 saved 事件，此事件发生在创建和编辑时、数据入库以后。
+    //在 saved() 方法中调用，确保了我们在分发任务时，$topic->id 永远有值。
+    public function saved(Topic $topic){
 
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if (! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
